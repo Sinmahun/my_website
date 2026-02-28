e60ad87dcdf4a4f094be4fdd766377a6cdd35d9f
Sinmahun <sinmahun2@gmail.com>
2026-02-28 11:56:36 +0700

    fix path2

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.post("/check", upload.single("image"), (req, res) => {
 
 /* ------------------ Health Check ------------------ */
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "homepage.html"));
 });
 
 /* ------------------ สำคัญมากสำหรับ Railway ------------------ */
